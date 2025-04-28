195cb9e838e2628249e4f7a8c88696ca8a1ae0db
Nur Aini Sri Oktaviani Bahar <nurainiannhy@gmail.com>
2025-04-28 20:55:39 +0800

    login regist dkk

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/forgotpass', function () {
+    return view('forgotpass');
+})->name('forgotpass');
+
+Route::get('/resetpass', function () {
+    return view('resetpass');
+})->name('resetpass');
+
+Route::get('/halal', function () {
+    return view('halal');
+})->name('halal');
