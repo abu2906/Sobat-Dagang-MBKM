5ac13bc76316b6b9c184b7aa995f8f5061592e8d
Andif12 <andifff12@gmail.com>
2025-05-21 05:28:49 +0800

    pengaduan user, fiks route (clear)

--- resources/views/component/navbar/user.blade.php
+++ resources/views/component/navbar/user.blade.php
@@ -1,45 +1,17 @@
-@auth
-<nav class="flex items-center justify-between p-4 text-white bg-primary">
-
-    <div class="flex items-center space-x-4">
-        <!-- Icon Notifikasi -->
-        <svg xmlns="http://www.w3.org/2000/svg" class="text-white cursor-pointer h-7 w-7 hover:text-gray-600"
-            fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002
-                   6.002 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67
-                   6.165 6 8.388 6 11v3.159c0 .538-.214
-                   1.055-.595 1.436L4 17h5m6 0v1a3 3 0
-                   11-6 0v-1m6 0H9" />
-        </svg>
-
-        <!-- Profil -->
-        <div x-data="{ showProfile: false }" class="relative">
-            <button @click="showProfile = true" class="text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                    class="text-white cursor-pointer h-7 w-7 hover:text-gray-600">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0
-                             a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15
-                             9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </svg>
-            </button>
-            @include('component.profile')
-        </div>
-    </div>
-</nav>
-@else
-<nav class="flex items-center justify-between p-4 text-white bg-primary">
+@auth('user')
+<nav class="navbar relative w-full px-4 py-2 text-white">
+    <div class="flex items-center justify-between w-full">
         <div class="navbar-left">
-        <a href="{{ route('user.dashboard') }}">
-            <img src="{{ asset('assets/img/icon/logo.png') }}" alt="Logo" class="logo">
-        </a>
-    </div>
-    <div class="navbar-center">
-        <ul id="nav-menu" class="nav-menu">
+            <a href="{{ route('user.dashboard') }}">
+                <img src="{{ asset('assets/img/icon/logo.png') }}" alt="Logo" class="h-10">
+            </a>
+        </div>
+
+    <div id="nav-menu" class="navbar-center hidden md:flex flex-col md:flex-row md:items-center md:space-x-6 md:justify-center w-full">
+        <ul class="nav-menu flex flex-col md:flex-row">
             <li><a href="https://peraturan.bpk.go.id/"><strong>REGULASI</strong></a></li>
             <li><a href="{{ route('about') }}"><strong>ABOUT US</strong></a></li>
+
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle"><strong>PELAYANAN</strong> <span class="dropdown-icon">▼</span></a>
                 <ul class="dropdown-menu">
@@ -56,46 +28,66 @@
                     <li class="dropdown-submenu">
                         <a href="#"><strong>INDUSTRI</strong></a>
                         <ul class="submenu">
-                            <li><a href="#"><strong>Directory Book</strong></a></li>
-                            <li><a href="#"><strong>Data IKM</strong></a></li>
-                            <li><a href="#"><strong>Sertifikasi IKM</strong></a></li>
+                            <li><a href="{{route('bidangIndustri.formPermohonan')}}"><strong>Permohonan IKM Binaan</strong></a></li>
+                            <li><a href="{{route('halal')}}"><strong>Data Sertifikat Halal</strong></a></li>
                         </ul>
                     </li>
                     <li class="dropdown-submenu">
                         <a href="#"><strong>METROLOGI</strong></a>
                         <ul class="submenu">
-                            <li><a href="{{ route('administrasi-metrologi') }}"><strong>Surat Permohonan Tera/Teraulang</strong></a></li>
+                            <li><a href="{{ route('administrasi-metrologi') }}"><strong>Permohonan Tera/Teraulang</strong></a></li>
                             <li><a href="{{ route('directory-metrologi') }}"><strong>Alat Milik Saya</strong></a></li>
                         </ul>
                     </li>
-                    <li><a href="#"><strong>PERSURATAN</strong></a></li>
                 </ul>
             </li>
-            <li><a href="{{ route('pelaporan') }}"><strong>PELAPORAN</strong></a></li>
-            <li><a href="" id=""><strong>FAQ</strong></a></li>
+
+            <li><a href="#"><strong>PELAPORAN</strong></a></li>
+            <li><a href="#" id="open-chat"><strong>FAQ</strong></a></li>
         </ul>
     </div>
 
-    <div class="flex items-center space-x-4">
-        <svg xmlns="http://www.w3.org/2000/svg" class="text-white cursor-pointer h-7 w-7 hover:text-gray-600"
-            fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002
-                   6.002 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67
-                   6.165 6 8.388 6 11v3.159c0 .538-.214
-                   1.055-.595 1.436L4 17h5m6 0v1a3 3 0
-                   11-6 0v-1m6 0H9" />
-        </svg>
-        <button class="px-4 py-1 text-sm font-semibold text-white rounded-md">
-            <svg xmlns="http://www.w3.org/2000/svg" class="text-white cursor-pointer h-7 w-7 hover:text-gray-600"
-                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M5.121 17.804A8.966 8.966 0 0112 15c2.132 0 4.087.745 5.621
-                       1.996M15 10a3 3 0 11-6 0 3 3 0
-                       016 0zm6 2a9 9 0 11-18 0 9 9 0
-                       0118 0z" />
-            </svg>
-        </button>
+        <div class="flex items-center space-x-4">
+            <button aria-label="Notifikasi" class="focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-7 w-7 hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002
+                            6.002 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67
+                            6.165 6 8.388 6 11v3.159c0 .538-.214
+                            1.055-.595 1.436L4 17h5m6 0v1a3 3 0
+                            11-6 0v-1m6 0H9" />
+                </svg>
+            </button>
+
+            <button aria-label="Profil" class="focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-7 w-7 hover:text-gray-300" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0
+                            a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15
+                            9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+            </button>
+
+            <button id="burger-btn" class="md:hidden focus:outline-none">
+                <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            </button>
+        </div>
     </div>
 </nav>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const burgerBtn = document.getElementById('burger-btn');
+        const navMenu = document.getElementById('nav-menu');
+
+        if (burgerBtn && navMenu) {
+            burgerBtn.addEventListener('click', () => {
+                navMenu.classList.toggle('hidden');
+            });
+        }
+    });
+</script>
 @endauth
