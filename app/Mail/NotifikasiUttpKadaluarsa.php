1a4d537372cb54b1d5f547e0f6a328e5afa914f8
Aisyah2910 <127122373+Aisyah2910@users.noreply.github.com>
2025-05-22 13:56:59 +0800

    update notifikasi

--- app/Mail/NotifikasiUttpKadaluarsa.php
+++ app/Mail/NotifikasiUttpKadaluarsa.php
@@ -41,7 +41,7 @@ class NotifikasiUttpKadaluarsa extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: '/',
         );
     }
 
