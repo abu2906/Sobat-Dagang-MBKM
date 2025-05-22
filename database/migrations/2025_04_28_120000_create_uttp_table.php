b7a65cf7d0907f65256ab0d59b3dc026eef77622
Noel <noel7ivander@gmail.com>
2025-05-23 00:01:53 +0800

    model dan database sertifikat

--- database/migrations/2025_04_28_120000_create_uttp_table.php
+++ database/migrations/2025_04_28_120000_create_uttp_table.php
@@ -30,6 +30,7 @@ class CreateUttpTable extends Migration
             $table->date('tanggal_selesai')->nullable();
             $table->string('terapan')->nullable();
             $table->text('keterangan')->nullable();
+            $table->string('sertifikat_path')->nullable();
             $table->timestamps();
         });
     }
