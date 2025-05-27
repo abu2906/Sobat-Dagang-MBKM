27f96bafadbd55a2a83a1de9d7807e603ee9a4d2
Andif12 <andifff12@gmail.com>
2025-05-27 20:40:58 +0800

    tipis2

--- resources/views/admin/adminSuper/manajemen_admin.blade.php
+++ resources/views/admin/adminSuper/manajemen_admin.blade.php
@@ -38,7 +38,7 @@
             <thead>
                 <tr class="bg-[#083358] text-white font-semibold">
                     <th class="px-4 py-3 border-b rounded-tl-xl">No</th>
-                                        <th class="px-4 py-3 border-b">NIP</th>
+                    <th class="px-4 py-3 border-b">NIP</th>
                     <th class="px-4 py-3 border-b">Email</th>
                     <th class="px-4 py-3 border-b">No. Telepon</th>
                     <th class="px-4 py-3 border-b">Role</th>
@@ -49,7 +49,6 @@
                 @php
                     $counter = 1;
                 @endphp
-                {{-- Disdag Users --}}
                 @foreach($disdagUsers as $user)
                 <tr class="hover:bg-gray-50">
                     <td class="px-4 py-2 text-center">{{ $counter++ }}</td>
