c06fef9c82d5d436a2bc658e56f9e6a7f6de3725
Andif12 <andifff12@gmail.com>
2025-06-03 22:30:29 +0800

    hmm

--- resources/views/pages/auth/verification.blade.php
+++ resources/views/pages/auth/verification.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-  
+  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('/assets/css/app.css') }}">
 </head>
 <body class="bg-primary min-h-screen flex flex-col justify-center items-center font-sans text-white">
