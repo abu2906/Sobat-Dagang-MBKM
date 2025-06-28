70ecaede980ea84bda8f769e18c1b50d89dba20f
Andif12 <andifff12@gmail.com>
2025-06-28 23:02:20 +0800

    verif api nya ku add kondisi

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ Route::get('/kategori', [ApiPerdaganganController::class, 'getKategori']);
 Route::get('/barang', [ApiPerdaganganController::class, 'getBarang']);
 Route::get('/indeks-harga', [ApiPerdaganganController::class, 'getIndeksHarga']);
 Route::get('/lokasi', [ApiPerdaganganController::class, 'getLokasi']);
+Route::get('/analisis/top-harga-naik', [ApiPerdaganganController::class, 'getTopHargaNaik']);
+Route::get('/analisis/top-harga-turun', [ApiPerdaganganController::class, 'getTopHargaTurun']);
 
 
 //Industri
